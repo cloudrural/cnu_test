f2b6734115532d62a5603bae2fe30e30fb9f94d6
Bharath Pantala <bpantala@securonix.com>
2021-12-05 20:22:01 +0530

    new patch

---
+++ merge.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# git-interactive-merge
+from=$1
+to=$2
+git checkout $from
+git checkout -b ${from}_tmp
+git rebase -i $to
+# Above will drop you in an editor and pick the changes you want
+git checkout $to
+git pull . ${from}_tmp
+git branch -d ${from}_tmp
+
+
+
+# git checkout
+
+
+git checkout master
+git checkout exp1 path/to/file_a
+git checkout exp2 path/to/file_b
+
+# Save these files as a stash
+git stash
+
+# Merge stash with master
+git merge stash
